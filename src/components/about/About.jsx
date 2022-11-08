aaa679cd98e583ce614058cdcfbd7850d8900517
Dipocbd <dipoojo6@gmail.com>
2022-11-08 03:51:21 -0800

    Added more Codes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,20 +6,34 @@ const About = () => {
   return (
     <>
     <div className='about_container'>
-        <div className='one'>
-        <div className='about_img'>
-        <img src ={IMG} alt="About Image"/>
-        <h4>Google Certified</h4>
-        </div>
+        <div className='about__left'>
+         <div className='about_img'>
+            <img src ={IMG} alt="About"/>
+           <h4>Google Certified</h4>
+          </div>
+       </div>
        
-        </div>
-       
-        <div className='about_content'>
+        <div className='about__right'>
          <h1 className='gradient_text'>ABOUT ME</h1>
-         <h3>A marketing and product design expert with a solid history of achievements in leading work-streams within cross-functional teams 
-            and driving projects proven to develop brands, products and excellent bottom-line results. 
-            A strategic thinker and detailed executioner with extensive experience in research,
-             user satisfaction analysis, design and business development.</h3>
+         <h4>A marketing and product design expert with a solid history of achievements in leading work-streams
+               within cross-functional teams 
+             and driving projects proven to develop brands, products and excellent bottom-line results. 
+             A strategic thinker and detailed executioner with extensive experience in research,
+              user satisfaction analysis, design and business development.
+            </h4>
+            <div className='about_content'>
+            <div className='about_details'>
+                <h3 className="green">5+</h3>
+                <h2>Years of 
+                Experience
+                </h2>
+            </div>
+            <div className='about_details'>
+                <h3 className='green'>47</h3>
+                <h2>Projects till date</h2>
+            </div>
+            </div>
+              
         </div>
     </div>
     </>
